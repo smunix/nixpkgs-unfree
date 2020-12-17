47374767f5366c690863264ba0ecf2eb820253a4
Providence Salumu <Providence.Salumu@smunix.com>
2020-12-17 08:14:44 -0500

    flake: working with zoom-us, google-chrome-dev

--- flake.nix
+++ flake.nix
@@ -1,9 +1,19 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux = import nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-    };
+  description = "Flake utils demo";
+
+  inputs.nixpkgs.url = "github:NixOs/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  
+  outputs = { self, nixpkgs, flake-utils }: {
+    packages.x86_64-linux =
+      with import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      {
+        inherit (pkgs) google-chrome-dev;
+        inherit (pkgs) spotify;
+        inherit (pkgs) zoom-us;
+      };
   };
 }
