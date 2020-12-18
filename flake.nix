e30d1836b752eac5d050a8e8b44719cbd983d7ab
Providence Salumu <Providence.Salumu@smunix.com>
2020-12-18 08:24:34 -0500

    clean up, add support for more systems

--- flake.nix
+++ flake.nix
@@ -1,22 +1,31 @@
 {
-  description = "Flake utils demo";
+  description = "Nixpkgs / allowUnfree ";
 
   inputs.nixpkgs.url = "github:NixOs/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  
-  outputs = { self, nixpkgs, flake-utils }: {
-    packages.x86_64-linux =
-      with import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-      {
-        inherit (pkgs) citrix_workspace;
-        inherit (pkgs) google-chrome;
-        inherit (pkgs) google-chrome-dev;
-        inherit (pkgs) google-chrome-beta;
-        inherit (pkgs) spotify;
-        inherit (pkgs) zoom-us;
-      };
-  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          inherit (pkgs) hello;
+          inherit (pkgs) citrix_workspace;
+          inherit (pkgs) google-chrome;
+          inherit (pkgs) google-chrome-dev;
+          inherit (pkgs) google-chrome-beta;
+          inherit (pkgs) spotify;
+          inherit (pkgs) zoom-us;
+          # inherit (pkgs) gitAndTools;
+        };
+        defaultPackage = packages.hello;
+        apps.hello = flake-utils.lib.mkApp { drv = packages.hello; };
+        defaultApp = apps.hello;
+      }
+    );
 }
+
