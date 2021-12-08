7bca991999ce235fbea3df228c40cbf8ddc92b35
Providence Salumu <Providence.Salumu@smunix.com>
2021-12-08 11:04:40 -0500

    vscode: added fhs

--- flake.nix
+++ flake.nix
@@ -8,58 +8,39 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true;
-              permittedInsecurePackages = [
-                "xpdf-4.02"
-              ];
-            };
+          inherit system;
+          config = {
+            allowUnfree = true;
+            permittedInsecurePackages = [ "xpdf-4.02" ];
           };
+        };
       in rec {
-        packages = flake-utils.lib.flattenTree ( with pkgs; recurseIntoAttrs {
-          web = recurseIntoAttrs {
-            chrome = recurseIntoAttrs {
-              stable = google-chrome;
-              beta = google-chrome-beta;
-              unstable = google-chrome-dev;
-            };
-          };
-          music = recurseIntoAttrs {
-            inherit
-              spotify
-            ;
-          };
-          office = recurseIntoAttrs {
-            inherit
-              wpsoffice
-            ;
-          };
-          talk = recurseIntoAttrs {
-            inherit
-              discord 
-              zoom-us
-            ;
-          };
-          remote = recurseIntoAttrs {
-            inherit
-              citrix_workspace
-            ;
-          };
-          dev = recurseIntoAttrs {
-            ide = recurseIntoAttrs {
-              code = recurseIntoAttrs {
-                inherit vscode;
-                inherit (vscode-extensions.alanz) vscode-hie-server;
-                inherit (vscode-extensions.haskell) haskell;
-                inherit (vscode-extensions.ms-vscode) cpptools;
-                inherit (vscode-extensions.ms-vsliveshare) vsliveshare;
+        packages = flake-utils.lib.flattenTree (with pkgs;
+          recurseIntoAttrs {
+            web = recurseIntoAttrs {
+              chrome = recurseIntoAttrs {
+                stable = google-chrome;
+                beta = google-chrome-beta;
+                unstable = google-chrome-dev;
               };
             };
-          };
-          # inherit gitAndTools;
-        });
-      }
-    );
+            music = recurseIntoAttrs { inherit spotify; };
+            office = recurseIntoAttrs { inherit wpsoffice; };
+            talk = recurseIntoAttrs { inherit discord zoom-us; };
+            remote = recurseIntoAttrs { inherit citrix_workspace; };
+            dev = recurseIntoAttrs {
+              ide = recurseIntoAttrs {
+                code = recurseIntoAttrs {
+                  inherit vscode;
+                  inherit vscode-fhs;
+                  inherit (vscode-extensions.alanz) vscode-hie-server;
+                  inherit (vscode-extensions.haskell) haskell;
+                  inherit (vscode-extensions.ms-vscode) cpptools;
+                  inherit (vscode-extensions.ms-vsliveshare) vsliveshare;
+                };
+              };
+            };
+            # inherit gitAndTools;
+          });
+      });
 }
-
